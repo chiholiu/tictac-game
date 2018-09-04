33e4015b93c43a373c6954a183397cb7dcf2f532
Chi Ho <chiho@chis-air.home>
2018-09-04 19:15:09 +0200

    html tictac board

---
+++ src/js/javascript.js
@@ -0,0 +1,40 @@
+(function() {
+    "use strict";
+    console.log('hello');
+
+
+    function TicTac() {
+        let checkValue;
+        let inputClassName= document.querySelectorAll('.choose-tictac');
+        let board = document.createElement('table');
+        
+       
+        this.init = function() {
+            printBoard();
+            addEventListener();
+        }
+
+        var printBoard = function() {
+
+        }
+
+        var addEventListener = function () {
+
+        }
+
+        var checkSymbol = function() {
+
+        }
+
+        var chooseItem = function(char) {
+           
+        } 
+
+        var getValueInput = function() {
+
+        }
+    }
+    
+    let tictac = new TicTac();
+    tictac.init();
+})();
